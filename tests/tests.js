039175d5100574350f95d496233ddc95e70c14db
Paul Scarrone <pscarrone@thinkthroughmath.com>
2014-11-19 08:17:48 -0500

    tests for param filtering

--- tests/tests.js
+++ tests/tests.js
@@ -1,12 +1,46 @@
 var should = require('should');
-var server = require('../server');
+var options = require('../options');
 
-describe('Creating a new User',function(){
-  var user = '';
+describe('options module',function(){
+  var simpleParams = {
+    "expr": [
+      "a = 1.2 * (2 + 4.5)",
+    ],
+    "precision": 14,
+    "notation": "auto",
+    "exponential": {"upper": 10, "lower": 12}
+  };
 
-  it('should have a username',function(){
-    console.log(JSON.stringify(server,null,2));
-    //options({"thing":1});
-    user.should.be.exactly('');
+  var scaleParams = {
+    "expr": [
+      "a = 1.2 * (2 + 4.5)"
+    ],
+    "scale": 14
+  };
+
+  var significantParams = {
+    "expr": [
+      "a = 1.2 * (2 + 4.5)",
+    ],
+    "significantDigits": 14
+  };
+
+  it('filters expr and ignores standard params',function(){
+    var filtered = options.filter(simpleParams);
+    filtered.should.eql({
+      "precision": 14,
+      "notation": "auto",
+      "exponential": {"upper": 10, "lower": 12}
+    });
+  });
+
+  it('converts scale to fixed decimal precision', function(){
+    var filtered = options.filter(scaleParams);
+    filtered.should.eql({"precision": 14, "notation": "fixed"});
+  });
+
+  it('converts significantDigits to digit precision', function(){
+    var filtered = options.filter(significantParams);
+    filtered.should.be.exactly(14);
   });
 });
